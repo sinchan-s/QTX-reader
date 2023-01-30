d876bafdc797ca161a38584ab2e202a44e034156
Sinchan <sinchan.tex@gmail.com>
2023-01-31 00:12:34 +0530

    release #1.7

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 import streamlit as st
 from io import StringIO
 
-# basic configurations
+#! basic configurations
 st.set_page_config(
     page_title="QTX reader",
     page_icon="🔹",
@@ -14,26 +14,28 @@ st.set_page_config(
 st.header("QTX file reader & color-graph display")
 
 
-# QTX file uploader
+#! QTX file uploader
 qtx_file = st.file_uploader("Upload QTX format file only:", type=['qtx'], accept_multiple_files=True, help="Supports multiple files upload")
-#st.write(qtx_file)
-# QTX file opener
+d65 = pd.read_csv('d65.csv').set_index('wavelength')
+inca = pd.read_csv('inca.csv').set_index('wavelength')
+merged_illum = d65.join(inca, on='wavelength', how='left', lsuffix='_D65', rsuffix='_INCA')
+#st.dataframe(merged_illum)
+#! QTX file opener
 try:
-    # converting qtx data to raw string
-    
+    #! converting qtx data to raw string
     with st.expander('Raw data: ', expanded=False):
         string_data = ""
         for f in qtx_file:
             stringio = StringIO(f.getvalue().decode("utf-8"))
             string_data = string_data + '\n' + stringio.read()
         st.write(string_data)
-    # values extraction using regex
+    #! values extraction using regex
     std_name = re.findall("STD_NAME=(.+)", string_data)
     list_ref_low = re.findall("STD_REFLLOW=(\d+),", string_data)
     list_ref_pts = re.findall("STD_REFLPOINTS=(\d+),", string_data)
     list_ref_inter = re.findall("STD_REFLINTERVAL=(\d+),", string_data)
     list_ref_vals = re.findall("STD_R[=,](.+)", string_data)
-    # color std selection & graph display
+    #! color std selection & graph display
     name_select = st.selectbox("Select Color", std_name)
     std_i = std_name.index(name_select)
     ref_low, ref_pts, ref_inter = int(list_ref_low[std_i]), int(list_ref_pts[std_i]), int(list_ref_inter[std_i])
@@ -42,8 +44,11 @@ try:
     x_wave_list = list(range(ref_low, ref_high, ref_inter))
     sd_df = pd.DataFrame(y_ref_val_list, index=x_wave_list, columns=[name_select])
     sd_df[name_select] = sd_df[name_select].astype('float64')
+    combi_df = sd_df.join(merged_illum, how='left', lsuffix='_std', rsuffix='_illum')
+    #st.dataframe(combi_df)
+    #! columnized sections
     col1, col2 = st.columns(2)
-    col1.area_chart(sd_df)
+    col1.line_chart(combi_df)
     color = col2.color_picker('Standard Color', '#ffffff')
 except:
     st.write("Waiting for your upload !")
