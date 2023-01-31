9bea36fee8e70e59cd6dd3f3068c0bf83ca2e544
Sinchan <sinchan.tex@gmail.com>
2023-01-31 23:45:12 +0530

    release #1.8

--- main.py
+++ main.py
@@ -18,27 +18,18 @@ st.header("QTX file reader & color-graph display")
 qtx_file = st.file_uploader("Upload QTX format file only:", type=['qtx'], accept_multiple_files=True, help="Supports multiple files upload")
 
 #! std illuminants for comparison
-def illum(name):
-    df = pd.read_csv(name).set_index('wavelength')
-    df_max = df['ref_val'].max()
-    df['std_'] = df['ref_val'].apply(lambda x:x*100/df_max)
-    df = df.drop('ref_val', axis=1)
-    return df
-
-d65 = illum('d65.csv')
-inca = illum('inca.csv')
-merged_illum = d65.join(inca, on='wavelength', how='right', lsuffix='D65', rsuffix='INCA')
+d65 = pd.read_csv('d65.csv').set_index('wavelength')
+inca = pd.read_csv('inca.csv').set_index('wavelength')
+merged_illum = d65.join(inca, on='wavelength', how='right', lsuffix='_D65', rsuffix='_INCA')
 #st.dataframe(inca)
 
 #! QTX file opener
 try:
     #! converting qtx data to raw string
-    with st.expander('Raw data: ', expanded=False):
-        string_data = ""
-        for f in qtx_file:
-            stringio = StringIO(f.getvalue().decode("utf-8"))
-            string_data = string_data + '\n' + stringio.read()
-        st.write(string_data)
+    string_data = ""
+    for f in qtx_file:
+        stringio = StringIO(f.getvalue().decode("utf-8"))
+        string_data = string_data + '\n' + stringio.read()
     #! values extraction using regex
     std_name = re.findall("STD_NAME=(.+)", string_data)
     list_ref_low = re.findall("STD_REFLLOW=(\d+),", string_data)
@@ -46,7 +37,7 @@ try:
     list_ref_inter = re.findall("STD_REFLINTERVAL=(\d+),", string_data)
     list_ref_vals = re.findall("STD_R[=,](.+)", string_data)
     #! color std selection & graph display
-    name_select = st.selectbox("Select Color", std_name)
+    name_select = st.selectbox("Select Color Std", std_name)
     std_i = std_name.index(name_select)
     ref_low, ref_pts, ref_inter = int(list_ref_low[std_i]), int(list_ref_pts[std_i]), int(list_ref_inter[std_i])
     ref_high = ref_low + ref_pts * ref_inter
@@ -54,11 +45,18 @@ try:
     x_wave_list = list(range(ref_low, ref_high, ref_inter))
     sd_df = pd.DataFrame(y_ref_val_list, index=x_wave_list, columns=[name_select])
     sd_df[name_select] = sd_df[name_select].astype('float64')
-    combi_df = merged_illum.join(sd_df, how='right', lsuffix='_std', rsuffix='_illum')
-    st.dataframe(combi_df)
+    combi_df = merged_illum.join(sd_df, how='right')
+    #st.dataframe(combi_df)
     #! columnized sections
     col1, col2 = st.columns(2)
-    col1.line_chart(combi_df)
-    color = col2.color_picker('Standard Color', '#ffffff')
+    if st.checkbox("relative to std"):
+        col_modify = [col for col in combi_df.columns if 'ref_val' in col]
+        combi_df['illum_D65'] = combi_df['ref_val_D65'].apply(lambda x:x*100/combi_df['ref_val_D65'].max())
+        combi_df['illum_INCA'] = combi_df['ref_val_INCA'].apply(lambda x:x*100/combi_df['ref_val_INCA'].max())
+        combi_df = combi_df.drop(['ref_val_D65', 'ref_val_INCA'], axis=1)
+    st.line_chart(combi_df)
+    #color = st.color_picker('Standard Color', '#ffffff')
+    with st.expander('Raw data: ', expanded=False):
+        st.write(string_data)
 except:
     st.write("Waiting for your upload !")
