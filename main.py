00e9fc72e40970005798ae1ffecbe9a76e2c655f
Sinchan <sinchan.tex@gmail.com>
2023-02-02 17:09:44 +0530

    release #1.9.1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ try:
     list_ref_inter = re.findall("STD_REFLINTERVAL=(\d+),", string_data)
     list_ref_vals = re.findall("STD_R[=,](.+)", string_data)
     #! color std selection & pre-display processing section
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     name_select = col1.selectbox("Select Color Std", std_name)
     std_i = std_name.index(name_select)
     ref_low, ref_pts, ref_inter = int(list_ref_low[std_i]), int(list_ref_pts[std_i]), int(list_ref_inter[std_i])
@@ -48,18 +48,18 @@ try:
     sd_df[name_select] = sd_df[name_select].astype('float64')
     combi_df = merged_illum.join(sd_df, how='right')
     color = col2.color_picker('Color Display (wip)', '#ffffff')
-    #st.dataframe(combi_df)
     #! data display section    
     if col2.checkbox("Illuminant relative to std"):
         for col in combi_df.columns:
             if 'ref_val' in col:
-                col_new = 'illum'+col[7:]
+                col_new = col[8:]+'_relative'
                 combi_df[col_new] = combi_df[col].apply(lambda x:x*100/combi_df[col].max())
                 combi_df = combi_df.drop(col, axis=1)
     if col1.checkbox("Show Illuminants"):
-        st.line_chart(combi_df)
+        col2.line_chart(combi_df)
     else:
-        st.line_chart(sd_df)
+        col2.line_chart(sd_df)
+    col1.dataframe(combi_df)
     #! qtx raw data display 
     with st.expander('Raw data: ', expanded=False):
         st.write(string_data)
