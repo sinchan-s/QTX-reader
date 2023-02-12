c06097c7b096270677a3c2111f62c20488852dd7
Sinchan <sinchan.tex@gmail.com>
2023-02-12 23:25:48 +0530

    release #1.9.11

--- main.py
+++ main.py
@@ -26,24 +26,37 @@ inca = pd.read_csv('inca.csv').set_index('wavelength')                          
 merged_illum = d65.join(inca, on='wavelength', how='right', lsuffix='_D65', rsuffix='_INCA')    #? merging D65 & INCA dataframes
 #st.dataframe(inca)
 
+def bokehFig(x_label, y_label, width, height, backfc, borderfc, outlc):
+    p = figure(width=width, height=height, background_fill_color=backfc, border_fill_color=borderfc, outline_line_color=outlc)
+    p.outline_line_color = "white"                                           #? plot styling elements
+    p.outline_line_width = 5
+    p.outline_line_alpha = 1
+    p.xaxis.axis_label = x_label
+    p.xaxis.axis_label_text_color = "white"
+    p.xaxis.major_label_text_color = "white"
+    p.yaxis.axis_label = y_label
+    p.yaxis.axis_label_text_color = "white"
+    p.yaxis.major_label_text_color = "white"
+    return
+
 #! QTX file opener
 try:
     #! converting qtx data to raw string
-    string_data = ""                                                                    #? empty variable for string holding
-    for f in qtx_file:                                                                  #? looping over every line in the file
-        stringio = StringIO(f.getvalue().decode("utf-8"))                               #? extracting & decoding every line to utf-8
-        string_data = string_data + '\n' + stringio.read()                              #? appending string variable
+    string_data = ""                                                        #? empty variable for string holding
+    for f in qtx_file:                                                      #? looping over every line in the file
+        stringio = StringIO(f.getvalue().decode("utf-8"))                   #? extracting & decoding every line to utf-8
+        string_data = string_data + '\n' + stringio.read()                  #? appending string variable
 
     #! qtx raw data display 
-    with st.expander('Raw data: ', expanded=False):                                     #? displaying raw data of color std
+    with st.expander('Raw data: ', expanded=False):                         #? displaying raw data of color std
         st.write(string_data)
     
     #! values extraction using regex
-    std_name = re.findall("STD_NAME=(.+)", string_data)                                 #? extracting the color stds name
-    list_ref_low = re.findall("STD_REFLLOW=(\d+),", string_data)                        #? lowest wavelength value
-    list_ref_pts = re.findall("STD_REFLPOINTS=(\d+),", string_data)                     #? total wavelength points
-    list_ref_inter = re.findall("STD_REFLINTERVAL=(\d+),", string_data)                 #? interval between wavelengths
-    list_ref_vals = re.findall("BAT_R=|STD_R=(.+)[,\s]", string_data)                          #? reflectance values match
+    std_name = re.findall("STD_NAME=(.+)", string_data)                     #? extracting the color stds name
+    list_ref_low = re.findall("STD_REFLLOW=(\d+),", string_data)            #? lowest wavelength value
+    list_ref_pts = re.findall("STD_REFLPOINTS=(\d+),", string_data)         #? total wavelength points
+    list_ref_inter = re.findall("STD_REFLINTERVAL=(\d+),", string_data)     #? interval between wavelengths
+    list_ref_vals = re.findall("BAT_R=|STD_R=(.+)[,\s]", string_data)       #? reflectance values match
     #st.write(list_ref_vals.strip(','))
 
     #! color std selection & pre-display processing section
@@ -59,51 +72,32 @@ try:
     combi_df = merged_illum.join(sd_df, how='right')                                    #? color display(wip)
     color = col2.color_picker('Color Display (wip)', '#ffffff')
 
-    #! Illuminant spectra relative to Color std spectra    
-    if col2.checkbox("Allign all"):                                                     #? checkbox for relative aligning of plots
-        for col in combi_df.columns:                                                    #? looping over every column in combi_df
-            if 'ref_val' in col:                                                        #? checking 'ref_val' columns
-                col_new = col[8:]+'_relative'                                           #? new name for the column
-                combi_df[col] = combi_df[col].apply(lambda x:x*100/combi_df[col].max()) #? applying the relative function on 'ref_val' columns
-
     #! Toggle to show illuminants
-    if col1.checkbox("Show Illuminants"):                                               #? checkbox for showing std illuminants
-        p = figure(width=600, height=300, background_fill_color="#0e1118", border_fill_color='#0e1118', outline_line_color='#ffffff', y_range=(0, 100))
-        p.outline_line_color = "white"                                                  #? plot styling elements
-        p.outline_line_width = 5
-        p.outline_line_alpha = 1
-        p.xaxis.axis_label = "Wavelength(λ)"
-        p.xaxis.axis_label_text_color = "white"
-        p.xaxis.major_label_text_color = "white"
-        p.yaxis.axis_label = "Reflectance(%)"
-        p.yaxis.axis_label_text_color = "white"
-        p.yaxis.major_label_text_color = "white"
-        x = combi_df.index                                                              #? assigning 'x' value
-        for i, j in enumerate(combi_df.columns):                                        #? looping over every column in combi_df
-            y = combi_df.iloc[:, i]                                                     #? assigning 'y' value
-            if j == combi_df.columns[-1]:                                               #? check for color std ref values column
-                p.line(x, y, line_width=3, color="#01f700")                             #? line plot
-            else:
-                p.line(x, y, line_width=2, color="#8b1302")
+    p = figure(width=600, height=300, background_fill_color="#0e1118", border_fill_color='#0e1118', outline_line_color='#ffffff', y_range=(0, 100))
+    p.outline_line_color = "white"                                          #? plot styling elements
+    p.outline_line_width = 5
+    p.outline_line_alpha = 1
+    p.xaxis.axis_label = "Wavelength(λ)"
+    p.xaxis.axis_label_text_color = "white"
+    p.xaxis.major_label_text_color = "white"
+    p.yaxis.axis_label = "Reflectance(%)"
+    p.yaxis.axis_label_text_color = "white"
+    p.yaxis.major_label_text_color = "white"
+    x = combi_df.index                                                      #? assigning 'x' value
+    if col1.checkbox("Hide Illuminants"):                                   #? checkbox for showing std illuminants
+        for i, col in enumerate(combi_df.columns):                          #? looping over every column in combi_df
+            if 'ref_val' in col:                                            #? checking 'ref_val' columns
+                combi_df[col] = combi_df[col].apply(lambda x:x*100/combi_df[col].max()) #? applying the relative function on 'ref_val' columns
+                y = combi_df.iloc[:, :i]
+        p.line(x, y, line_width=3, color="#01f700")
         col2.bokeh_chart(p)
     else:
-        p = figure(width=600, height=300, background_fill_color="#0e1118", border_fill_color='#0e1118', outline_line_color='#ffffff', y_range=(0, 100))
-        p.outline_line_color = "white"                                                  #? plot styling elements
-        p.outline_line_width = 5
-        p.outline_line_alpha = 1
-        p.xaxis.axis_label = "Wavelength(λ)"
-        p.xaxis.axis_label_text_color = "white"
-        p.xaxis.major_label_text_color = "white"
-        p.yaxis.axis_label = "Reflectance(%)"
-        p.yaxis.axis_label_text_color = "white"
-        p.yaxis.major_label_text_color = "white"
-        x = sd_df.index                                                                 #? assigning 'x' value
-        y = sd_df.iloc[:, 0]                                                            #? assigning 'y' value
-        p.line(x, y, line_width=3, color="#01f700")                                     #? line plot
+        y = sd_df.iloc[:, 0]                                                #? assigning 'y' value
+        p.line(x, y, line_width=3, color="#01f700")                         #? line plot
         col2.bokeh_chart(p)
 
-    with col1.expander('Table', expanded=False):                                        #? displaying dataframe
+    with col1.expander('Table', expanded=False):                            #? displaying dataframe
         st.dataframe(combi_df)
 
 except:
-    st.write("Waiting for your upload !")                                               #? exception handling
+    st.write("Waiting for your upload !")                                   #? exception handling
